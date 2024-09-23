3699c1b8835c3dc69a32117603b406303a82fd3a
chubao <1967795304@qq.com>
2024-09-23 14:38:38 +0800

    second commit, userProfile view

---
+++ src/components/UserProfileInfoView.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img src="../assets/logo.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-9">
+            <div class="username">Chu Bao</div>
+            <div class="fans">粉丝：9999</div>
+            <button type="button" class="btn btn-secondary btn-sm">+关注</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'UserProfileInfo',
+}
+
+</script>
+
+<style>
+img {
+    border-radius: 50%;
+}
+
+.usernam {
+    font-weight: bold;
+}
+
+.fans {
+    font-size: 12px;
+    color: gray;
+}
+
+button {
+    padding: 2px 4px;
+    font-size: 12px;
+}
+</style>
--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,16 +1,30 @@
 <template>
   <ContentBaseView>
-    UserProfile
+    <div class="row">
+      <div class="col-3">
+        <UserProfileInfo />
+      </div>
+      <div class="col-9">帖子列表</div>
+    </div>
   </ContentBaseView>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-9"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 import ContentBaseView from '../components/ContentBaseView.vue'
+import UserProfileInfo from '../components/UserProfileInfoView.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    ContentBaseView
+    ContentBaseView,
+    UserProfileInfo
   }
 }
 </script>
